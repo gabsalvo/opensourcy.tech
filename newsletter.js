d93568575960f437863f9fddf193d193a4f7d0c8
Gabriele Salvo <72619479+gabsalvo@users.noreply.github.com>
2022-10-09 17:15:03 +0100

    Delete newsletter.js

--- newsletter.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.submit-email').addEventListener('mousedown', (e) => {
-    e.preventDefault();
-    document.querySelector('.subscription').classList.add('done');
-    setTimeout(function() {
-      document.querySelector('.subscription').classList.remove('done');
-          }, 3000);
-  });
